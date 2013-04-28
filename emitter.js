8f69e36b80a013525514d079d577e725be279385
Nicolas Gallagher <nicolasgallagher@gmail.com>
2013-04-27 22:50:17 -0700

    Implement `getListeners` method
    Create the registry and events array if they are missing.
    Returns an array of callbacks registered for an event.

--- emitter.js
+++ emitter.js
@@ -8,6 +8,24 @@ function Emitter() {
 }
 
 /**
+ * Returns an array of callbacks for the specified event.
+ * The event registry will be initialized if required.
+ *
+ * @param {String} event
+ * @return {Function[]} callbacks
+ * @api public
+ */
+
+Emitter.prototype.getListeners = function (event) {
+    // get the registry; create it if missing
+    var registry = this._registry || (this._registry = {});
+    // get the array of callbacks for an event; create it if missing
+    var callbacks = registry[event] || (registry[event] = []);
+
+    return callbacks;
+};
+
+/**
  * Expose `Emitter`
  */
 
