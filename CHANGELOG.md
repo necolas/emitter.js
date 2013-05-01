0a2fdd70a9d6bb0873dac894d830b0c63fc502c0
Nicolas Gallagher <nicolasgallagher@gmail.com>
2013-04-30 18:49:02 -0700

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 == HEAD
 
+== 1.0.1 (May 1, 2013)
+
 * Remove ES5 Shim dependency.
 
 == 1.0.0 (April 28, 2013)
--- emitter.js
+++ emitter.js
@@ -1,4 +1,4 @@
-/*! emitter.js v1.0.0 - Nicolas Gallagher - MIT license */
+/*! emitter.js v1.0.1 - Nicolas Gallagher - MIT license */
 
 ;(function (global) {
 
